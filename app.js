77b164cacda3fda7185763c31e64b7988e95339d
mohit <mohitratan2003@gmail.com>
2025-01-13 00:01:18 +0530

    Added corsOptions

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 3000;
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
-app.use('/api/auth', require("./authController"));
+app.use("/api/auth", require("./authController"));
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
